f802f1ee5b0f6363e6b6ffdc66dbd5bc3b9a918b
Mahmud-Araf <almahmudaraf@gmail.com>
2023-04-05 00:42:50 +0600

    modified

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
