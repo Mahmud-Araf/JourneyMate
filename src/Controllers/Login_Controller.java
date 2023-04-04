8e3bdf7bb5f4a463db4d0d1b40503f37e5510903
Mahmud-Araf <almahmudaraf@gmail.com>
2023-04-05 01:39:59 +0600

    dashboard.fxml added

--- src/Controllers/Login_Controller.java
+++ src/Controllers/Login_Controller.java
@@ -4,16 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Login_Controller {
     @FXML
     private Stage stage;
-    
+
     public void clickSignUp(ActionEvent event) throws IOException {
-       Parent root = FXMLLoader.load(getClass().getResource("fxml/signup.fxml"));
+        AnchorPane  root = FXMLLoader.load(getClass().getResource("fxml/signup.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
