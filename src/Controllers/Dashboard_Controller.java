a121cb8aedb13d695e3472e4ff3fcb14d7a5af0c
Mahmud-Araf <almahmudaraf@gmail.com>
2023-04-06 17:19:49 +0600

    about screen content visible

--- src/Controllers/Dashboard_Controller.java
+++ src/Controllers/Dashboard_Controller.java
@@ -1,17 +1,19 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+
 
 public class Dashboard_Controller implements Initializable{
 
@@ -27,17 +29,27 @@ public class Dashboard_Controller implements Initializable{
     private ImageView DashboardImg;
     @FXML
     private AnchorPane SidebarPane;
+    @FXML
+    private AnchorPane ContentPane;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
        
+        
+        
         DashboardImg.setOnMouseClicked(event->
         {
             
-            if(SidebarPane.isVisible()==false)
-            {
-                SidebarPane.setVisible(true);
+            
 
+            if(SidebarPane.isVisible()==false)
+            {   
+                ContentPane.setLayoutX(200);
+                ContentPane.setMaxWidth(550);
+                ContentPane.setMinWidth(550);
+                ContentPane.setPrefWidth(550);
+                SidebarPane.setVisible(true);
+              
                 FadeTransition ft=new FadeTransition(Duration.seconds(0.5),SidebarPane);
                 ft.setFromValue(0);
                 ft.setToValue(1);
@@ -46,10 +58,15 @@ public class Dashboard_Controller implements Initializable{
                 TranslateTransition tt= new TranslateTransition(Duration.seconds(0.5),SidebarPane);
                 tt.setByX(+200);
                 tt.play();
+                
             }
             else
-            {
-                
+            {   
+                ContentPane.setLayoutX(0);
+                ContentPane.setMaxWidth(750);
+                ContentPane.setMinWidth(750);
+                ContentPane.setPrefWidth(750);
+            
                 FadeTransition ft=new FadeTransition(Duration.seconds(0.5),SidebarPane);
                 ft.setFromValue(1);
                 ft.setToValue(0);
@@ -64,16 +81,20 @@ public class Dashboard_Controller implements Initializable{
         });
     }
 
-    public void clickAboutButton(ActionEvent event)
-    {
-           AboutButton.setStyle("-fx-background-color:  #03fcf8;"+"-fx-background-radius:50;");
-           ClientsButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
-           PackagesButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
-           BookingButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
+    public void clickAboutButton (ActionEvent event) throws IOException
+    {      
+           
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("about.fxml"));
+        AnchorPane newPane = fxmlLoader.load();
+        ContentPane.getChildren().setAll(newPane);  
+        AboutButton.setStyle("-fx-background-color:  #03fcf8;"+"-fx-background-radius:50;");
+        ClientsButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
+        PackagesButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
+        BookingButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
     }
 
     public void clickClientsButton(ActionEvent event)
-    {
+    {      ContentPane.getChildren().clear();
            AboutButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
            ClientsButton.setStyle("-fx-background-color:  #03fcf8;"+"-fx-background-radius:50;");
            PackagesButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
@@ -81,7 +102,7 @@ public class Dashboard_Controller implements Initializable{
     }
 
     public void clickPackagesButton(ActionEvent event)
-    {
+    {      
            AboutButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
            ClientsButton.setStyle("-fx-background-color: #008080;"+"-fx-background-radius:50;");
            PackagesButton.setStyle("-fx-background-color:  #03fcf8;"+"-fx-background-radius:50;");
--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         AnchorPane root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
