03913529e0c93ffb2da27d6cdd59b57a8d580eca
Mahmud-Araf <almahmudaraf@gmail.com>
2023-05-01 11:41:14 +0600

    package adding is  completed

--- src/Controllers/PackageScreen_Controller.java
+++ src/Controllers/PackageScreen_Controller.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import Classes.User;
 import javafx.event.ActionEvent;
@@ -12,8 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 public class PackageScreen_Controller extends Basic_Controller implements ControllerFunctions{
 
@@ -26,6 +29,22 @@ public class PackageScreen_Controller extends Basic_Controller implements Contro
     private Label RangeLabel;
     @FXML
     private Button DelButton;
+
+    @FXML
+    private TextField PackageNameTextField;
+    @FXML
+    private TextField DistrictTextField;
+    @FXML
+    private TextField spotNameTextField;
+    @FXML
+    private TextField spotPriceTextField;
+
+    @FXML
+    private ListView<String>spotNameListView;
+    @FXML
+    private ListView<Integer>spotPriceListView;
+
+
 
     public String getTotalNumber(String type,String UserName)
     {
@@ -106,12 +125,8 @@ public class PackageScreen_Controller extends Basic_Controller implements Contro
     }
 
 
-
-
-
-
     @Override
-    protected void clickBackButton(ActionEvent event) {
+    public void clickBackButton(ActionEvent event) {
         try {
             changeScenewithBorderPane("dashboard.fxml", event, "Main Menu");
         } catch (Exception e) {
@@ -119,15 +134,137 @@ public class PackageScreen_Controller extends Basic_Controller implements Contro
         }
     }
 
+    @FXML
+    private void addSpotNameClicked(ActionEvent event) 
+    {
+        String text = spotNameTextField.getText();
+        if (!text.isEmpty()) 
+        {
+            spotNameListView.getItems().add(text);
+            spotNameTextField.clear();
+        }
+    }
 
+    @FXML
+    private void addSpotPriceClicked(ActionEvent event) 
+    {
+        String text = spotPriceTextField.getText();
+        if (!text.isEmpty()) 
+        {
+            spotPriceListView.getItems().add(Integer.parseInt(text));
+            spotPriceTextField.clear();
+        }
+    }
 
+    public void removeFromSpotName(ActionEvent event)
+    {
+        int selectedIndex = spotNameListView.getSelectionModel().getSelectedIndex();
+        if (selectedIndex != -1) {
+            spotNameListView.getItems().remove(selectedIndex);
+        }
+    }
 
+    public void removeFromSpotPrice(ActionEvent event)
+    {
+        int selectedIndex = spotPriceListView.getSelectionModel().getSelectedIndex();
+        if (selectedIndex != -1) 
+        {
+            spotPriceListView.getItems().remove(selectedIndex);
+        }
+    }
 
 
     @Override
     public void addAction(ActionEvent event) {
+        try {
+            changeScenewithAnchorPane("addPackage.fxml", event, "Main Menu");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
-        
+    }
+
+    @FXML
+    private void addPackageToDB(ActionEvent event)
+    {
+        startDB();
+        var spotNameArrayList = new ArrayList<String>();
+        var spotPriceArrayList =new ArrayList<Integer>();
+
+        String PackageNamestr = PackageNameTextField.getText();
+        String Districtstr = DistrictTextField.getText();
+
+        spotNameArrayList.addAll(spotNameListView.getItems());
+        spotPriceArrayList.addAll(spotPriceListView.getItems());
+
+        int SpotNameSize = spotNameArrayList.size();
+        int SpotPriceSize = spotPriceArrayList.size();
+
+        if(SpotNameSize==0||SpotNameSize!=SpotPriceSize||PackageNamestr==null ||Districtstr==null)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Provide all necessary information");
+            DialogPane dialogpane = alert.getDialogPane();
+            dialogpane.setStyle("-fx-background-color:#e36212;");
+            alert.show();
+        }
+        else
+        {
+            try {
+                    setConnection();
+                    preparedStatement2 = connection
+                            .prepareStatement("SELECT * FROM TourPackages WHERE UserName = ? AND PackageName = ? ");
+                    preparedStatement2.setString(1, User.Name);
+                    preparedStatement2.setString(2,PackageNamestr);
+                    resultSet = preparedStatement2.executeQuery();
+
+                    if (resultSet.isBeforeFirst())
+                    {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("Package already exists");
+                        DialogPane dialogpane = alert.getDialogPane();
+                        dialogpane.setStyle("-fx-background-color:#e36212;");
+                        alert.show();
+                    }
+                    else{
+
+                        int TotalPriceINT=0;
+
+                        for(int i=0;i<SpotPriceSize;i++)
+                        {
+                            TotalPriceINT+=spotPriceArrayList.get(i);
+                        }
+                        
+                        for(int i=0;i<SpotNameSize;i++)
+                        {
+                            preparedStatement1 = connection.prepareStatement(
+                            "INSERT INTO TourPackages (UserName,PackageName,District,SpotName,SpotPrice,TotalPrice) VALUES(?,?,?,?,?,?)");
+                            preparedStatement1.setString(1, User.Name);
+                            preparedStatement1.setString(2, PackageNamestr);
+                            preparedStatement1.setString(3, Districtstr);
+                            preparedStatement1.setString(4, spotNameArrayList.get(i));
+                            preparedStatement1.setInt(5, spotPriceArrayList.get(i));
+                            preparedStatement1.setInt(6, TotalPriceINT);
+                            preparedStatement1.executeUpdate();
+                        }
+
+                        try {
+                            changeScenewithBorderPane("dashboard.fxml", event, "Main Menu");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            } 
+            catch (SQLException e) {
+               e.printStackTrace();
+            }
+            finally
+            {
+                closeDB();
+            }
+
+        }
+
     }
 
 
